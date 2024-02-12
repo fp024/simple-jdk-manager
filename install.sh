70cf0c767ae8e92a34b4ea14bc806318d603cd89
fp024 <aopenasus@naver.com>
2024-02-12 22:16:22 +0900

    설치 스크립트 추가

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+JDK_SH_ROOT=$1
+
+if [ -z "$JDK_SH_ROOT" ]; then
+  echo "[알림] 단순 JDK 관리자 스크립트가 설치될 경로를 입력해주세요. 예) /usr/local/JDK"
+  exit 1
+fi
+
+sudo mkdir -p $JDK_SH_ROOT
+
+if [ ! -d "$JDK_SH_ROOT" ]; then
+  echo "[알림] 설치될 경로가 만들어지지 않았습니다.: $JDK_SH_ROOT"
+fi
+
+CURRENT_USER=$(whoami)
+CURRENT_GROUP=$(id -g -n)
+
+sudo chown $CURRENT_USER.$CURRENT_GROUP $JDK_SH_ROOT
+wget https://raw.githubusercontent.com/fp024/simple-jdk-manager/master/update.sh -O $JDK_SH_ROOT/update.sh || { echo "[오류] update.sh 파일 다운로드에 실패했습니다."; exit 1; }
+
+chmod u+x $JDK_SH_ROOT/update.sh
+
